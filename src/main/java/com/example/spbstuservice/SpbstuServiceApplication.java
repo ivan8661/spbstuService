56260dee36d69d95b6d55820546998fc6babe2f0
ivan8661 <9vp4ss482305iv>
2022-01-30 23:55:56 +0300

    Initial commit

---
+++ src/main/java/com/example/spbstuservice/SpbstuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spbstuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbstuServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbstuServiceApplication.class, args);
+    }
+
+}
