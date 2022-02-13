bf5135871e9114201642226cf7af3fe8e155e24e
ivan8661 <9vp4ss482305iv>
2022-02-13 13:13:14 +0300

    + | Faculties | classes for import from faculties + | Groups | classes for import groups * | Professors | change inner class + | Teachers | classes for import professors

---
+++ src/main/java/restfulapi/api/spbstuservice/Services/importLessons/Entities/Groups.java
@@ -0,0 +1,214 @@
+package restfulapi.api.spbstuservice.Services.importLessons.Entities;
+
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "level",
+        "type",
+        "kind",
+        "spec",
+        "year"
+})
+
+public class Groups {
+
+    @JsonProperty("groups")
+    private List<Group> groups = null;
+    @JsonProperty("faculty")
+    private Faculty faculty;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("groups")
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    @JsonProperty("groups")
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    @JsonProperty("faculty")
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    @JsonProperty("faculty")
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public static class Group {
+
+        @JsonProperty("id")
+        private Integer id;
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("level")
+        private Integer level;
+        @JsonProperty("type")
+        private String type;
+        @JsonProperty("kind")
+        private Integer kind;
+        @JsonProperty("spec")
+        private String spec;
+        @JsonProperty("year")
+        private Integer year;
+        @JsonIgnore
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+        @JsonProperty("id")
+        public Integer getId() {
+            return id;
+        }
+
+        @JsonProperty("id")
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        @JsonProperty("name")
+        public String getName() {
+            return name;
+        }
+
+        @JsonProperty("name")
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @JsonProperty("level")
+        public Integer getLevel() {
+            return level;
+        }
+
+        @JsonProperty("level")
+        public void setLevel(Integer level) {
+            this.level = level;
+        }
+
+        @JsonProperty("type")
+        public String getType() {
+            return type;
+        }
+
+        @JsonProperty("type")
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        @JsonProperty("kind")
+        public Integer getKind() {
+            return kind;
+        }
+
+        @JsonProperty("kind")
+        public void setKind(Integer kind) {
+            this.kind = kind;
+        }
+
+        @JsonProperty("spec")
+        public String getSpec() {
+            return spec;
+        }
+
+        @JsonProperty("spec")
+        public void setSpec(String spec) {
+            this.spec = spec;
+        }
+
+        @JsonProperty("year")
+        public Integer getYear() {
+            return year;
+        }
+
+        @JsonProperty("year")
+        public void setYear(Integer year) {
+            this.year = year;
+        }
+
+        @JsonAnyGetter
+        public Map<String, Object> getAdditionalProperties() {
+            return this.additionalProperties;
+        }
+
+        @JsonAnySetter
+        public void setAdditionalProperty(String name, Object value) {
+            this.additionalProperties.put(name, value);
+        }
+
+    }
+
+    public static class Faculty {
+
+        @JsonProperty("id")
+        private Integer id;
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("abbr")
+        private String abbr;
+        @JsonIgnore
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+        @JsonProperty("id")
+        public Integer getId() {
+            return id;
+        }
+
+        @JsonProperty("id")
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        @JsonProperty("name")
+        public String getName() {
+            return name;
+        }
+
+        @JsonProperty("name")
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @JsonProperty("abbr")
+        public String getAbbr() {
+            return abbr;
+        }
+
+        @JsonProperty("abbr")
+        public void setAbbr(String abbr) {
+            this.abbr = abbr;
+        }
+
+        @JsonAnyGetter
+        public Map<String, Object> getAdditionalProperties() {
+            return this.additionalProperties;
+        }
+
+        @JsonAnySetter
+        public void setAdditionalProperty(String name, Object value) {
+            this.additionalProperties.put(name, value);
+        }
+
+    }
+}
--- src/main/java/restfulapi/api/spbstuservice/Services/importLessons/ImportService.java
+++ src/main/java/restfulapi/api/spbstuservice/Services/importLessons/ImportService.java
@@ -1,26 +1,78 @@
 package restfulapi.api.spbstuservice.Services.importLessons;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import restfulapi.api.spbstuservice.Entities.DatabaseEntities.Professor;
+import restfulapi.api.spbstuservice.Exceptions.UserException;
+import restfulapi.api.spbstuservice.Exceptions.UserExceptionType;
+import restfulapi.api.spbstuservice.Repositories.BuildingRepository;
+import restfulapi.api.spbstuservice.Repositories.ProfessorRepository;
+import restfulapi.api.spbstuservice.Services.importLessons.Entities.Faculties;
+import restfulapi.api.spbstuservice.Services.importLessons.Entities.Groups;
+import restfulapi.api.spbstuservice.Services.importLessons.Entities.Teachers;
+import restfulapi.api.spbstuservice.SpbstuServiceApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ImportService {
 
 
+    public ProfessorRepository professorRepository;
+    public BuildingRepository buildingRepository;
 
-
-
-
-    private void importTeachers() {
-
+    @Autowired
+    public ImportService(ProfessorRepository professorRepository, BuildingRepository buildingRepository) {
+        this.professorRepository = professorRepository;
+        this.buildingRepository = buildingRepository;
     }
 
-    private void importGroups() {
 
+    public void importTeachers() throws UserException {
+        Teachers teachers = new RestTemplate().getForObject("https://ruz.spbstu.ru/api/v1/ruz/teachers", Teachers.class);
+        if (teachers == null || teachers.getTeachers().isEmpty())
+            throw new UserException(UserExceptionType.SERVER_ERROR, "teachers in import are missing!");
+        List<Professor> professors = teachers
+                .getTeachers()
+                .stream()
+                .map(Professor::new)
+                .collect(Collectors.toList());
+        professorRepository.saveAll(professors);
     }
 
-    private void importGroups() {
+    public void importGroups() throws UserException {
+        Faculties faculties = new RestTemplate().getForObject("https://ruz.spbstu.ru/api/v1/ruz/faculties", Faculties.class);
+        if (faculties == null || faculties.getFaculties().isEmpty())
+            throw new UserException(UserExceptionType.SERVER_ERROR, "faculties in import are missing!");
+        List<Integer> facultiesId = faculties
+                .getFaculties()
+                .stream()
+                .map(Faculties.Faculty::getId)
+                .collect(Collectors.toList());
+        for (int facultyId : facultiesId) {
+            new Thread(() -> importThreadGroups(facultyId)).start();
+        }
 
     }
+//
+//    private void importGroups() {
+//
+//    }
 
+
+    private void importThreadGroups(int facultyId) {
+        Groups groups = new RestTemplate().getForObject("https://ruz.spbstu.ru/api/v1/ruz/faculties/"+ facultyId + "/groups", Groups.class);
+        
+    }
 }
