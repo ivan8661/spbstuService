57b77fb097f4f28f2a726a7e93e0ffc0af9ff1ec
god <ivan@poltorakov.ru>
2022-03-30 19:44:43 +0300

    + | ErrorHandler | template from previous project

--- src/main/java/restfulapi/api/spbstuservice/SpbstuServiceApplication.java
+++ src/main/java/restfulapi/api/spbstuservice/SpbstuServiceApplication.java
@@ -1,6 +1,5 @@
 package restfulapi.api.spbstuservice;
 
-import io.netty.internal.tcnative.AsyncTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +35,6 @@ public class SpbstuServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpbstuServiceApplication.class, args);
-
     }
 
 
